25d885b93b01198b9a0f56bc5e4cd280d21b343a
Slay <destpoch@mail.ru>
2022-07-19 18:22:43 +0300

    depl

--- My_INSTA/src/App.js
+++ My_INSTA/src/App.js
@@ -13,18 +13,19 @@ import VkLoginName from './modules/WelcomePage/VkLogin/VkLoginName';
 import UserVideoArchive from './components/pages/userVideoArchive/userVideoArchive';
 
 
-
+const ENV = 'myinsta_prod/'
+const ENV_CLEAN = 'myinsta_prod'
 
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Navigate replace to='login/'/>}/>
-        <Route path='main/' element={<MainPage/>} />
+        <Route path={ENV} element={<Navigate replace to='myinsta_prod/login/'/>}/>
+        <Route path='myinsta_prod/main/' element={<MainPage/>} />
         <Route path='lk/' element={<UserCabinet/>} />
         <Route path="video/:id" element={<VideoPostPage/>}/>
-        <Route path='login/' element={<WelcomePage/>} />
+        <Route path='myinsta_prod/login/' element={<WelcomePage/>} />
         <Route path='login/registration/' element={<RegistrationPage/>} />
         <Route path='userclean/' element={<ClearUserService/>}/>
         <Route path='userVideoPage/' element={<UserVideoPage/>}/>
